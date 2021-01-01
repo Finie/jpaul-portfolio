6d348296d6363e71470780774464b9091d171eb6
unknown <finiephenius@gmail.com>
2021-01-02 02:24:08 +0300

    footer done profile started

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,103 @@
 import React from 'react'
+import {
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+    FaPhoneAlt,
+    FaHeart
+  } from "react-icons/fa";
 
 import './footer.css'
 
 function Footer(){
 return(
     <div id="contact" className="footer-container">
-        <h1>Footer</h1>
+        
+        <hr/>
+
+        <section className="footer-subscription">
+        <h2 className="footer-subscription-heading">Services</h2>
+        <div class="technologies">
+
+          <span>
+              <p>Copywriting</p>
+          </span>
+
+          <span>
+              <p>SEO writing</p>
+          </span>
+
+          <span>
+              <p>Artical writing</p>
+          </span>
+
+          <span>
+             <p>Proofreading</p>
+          </span>
+
+          <span>
+              <p>Research</p>
+          </span>
+
+        </div>
+      </section>
+
+
+
+
+      <div className="footer-links">
+
+        <div className="footer-link-wrapper">
+
+        <div className="footer-link-items">
+            <h2>Social Media</h2>
+
+            <a target="_blank" rel="noreferrer" href="http://www.linkedin.com/in/judie-paul-2015">
+              <FaLinkedin className="icons" /> LinkedIn
+            </a>
+         
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/judiepaul555">
+              <FaTwitter className="icons" /> Twitter
+            </a>
+
+            <a href="/">
+              <FaInstagram className="icons"/> Instagram
+            </a>
+
+           
+
+          </div>
+
+
+          <div className="footer-link-items">
+            <h2>Fields </h2>
+            <a href="/">Technology 
+            </a>
+            <a href="/">Blockchain 
+            </a>
+            <a href="/"> Cryptocurrency 
+            </a>
+            <a href="/">Finance Writer
+            </a>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contacts</h2>
+            <a href="tel:+254728790349"><FaPhoneAlt /> +254728790349 </a>
+            <a href="mailto:judiepaul55@gmail.com">judiepaul55@gmail.com</a>
+          </div> </div>
+
+      </div>
+
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <small className="website-rights"> Judie Paul © 2021 all rights reserved.   Built with <FaHeart style={{color:"red"}} /> by <a target="_blank" rel="noreferrer"  href="https://phenius-muthomi.web.app/" ><b>Fin</b></a> </small>
+          
+          <div className="social-icons">
+          </div>
+        </div>
+      </section>
+
+
     </div>
 );
 }
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import {
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
 
 import './profile.css'
 
@@ -7,7 +11,16 @@ import './profile.css'
 function Profile(){
     return(
         <div id="profile" className="profile-container">
-          <h1>Profile</h1>
+        <h1 className="logo-name">Judie <span style={{color:"#FFB23C"}}>Paul</span></h1>
+        <section className="logo-description"> <span style={{color:"#FFB23C"}}> | </span>Freelance writer for Technology  Blockchain  Cryptocurrency & Finance  <span style={{color:"#FFB23C"}}> | </span></section>
+       <div className="social-icon">
+         <a target="_blank" rel="noreferrer" href="http://www.linkedin.com/in/judie-paul-2015">   <FaLinkedin style={{ width:30,height:30}} /> </a>
+     <a target="_blank" rel="noreferrer" href="https://twitter.com/judiepaul555"><FaTwitter style={{ width:30,height:30}} /></a>
+     <a href="/"> <FaInstagram style={{ width:30,height:30}} /></a>
+       
+      
+
+       </div>
         </div>
     );
 }
